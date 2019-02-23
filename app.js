13adcd5c4e5e8abba23bcbed2769f37de8f483a8
DeanDK <deanbozic12@gmail.com>
2019-02-23 17:27:19 +0100

    addded init structure for graphql mutation and resolvers on express

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: buildSchema(`
+      type RootQuery {
+          events: [String!]!
+      }
+
+      type RootMutation {
+          createEvent(name: String): String
+      }
+
+      schema {
+        query: RootQuery
+        mutation: RootMutation
+      }
+    `),
+    rootValue: {
+      events: () => {
+        return ["test", "test1"];
+      },
+      createEvent: args => {
+        const eventName = args.name;
+        return eventName;
+      }
+    }
+  })
+);
+
 app.listen(3000);
